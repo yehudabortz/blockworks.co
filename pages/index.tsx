ac5c2053c60972048e819045cc873ce6bef4cae6
Yehuda Bortz <yehudabortz@icloud.com>
2023-06-21 11:37:48 -0400

    format

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,40 +6,40 @@ import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 
 export default function Home() {
-    const options = {
-  title: {
-    text: 'My chart'
-  },
-  series: [{
-    data: [1, 2, 3]
-  }]
-    }
-  
-    return (
-      <Layout home>
-    
-        <Head>
-          
-                <title>BTC Address Balances over Time</title>
-            </Head>
-            <section>
-                <div className="max-w-2xl mx-auto p-8 text-center">Chart goes here</div>
-                <HighchartsReact
-    highcharts={Highcharts}
-    options={options}
-          />
-        
-            </section>
-        </Layout>
-    )
+  const options = {
+    title: {
+      text: 'My chart'
+    },
+    series: [{
+      data: [1, 2, 3]
+    }]
+  }
 
-  
+  return (
+    <Layout home>
+
+      <Head>
+
+        <title>BTC Address Balances over Time</title>
+      </Head>
+      <section>
+        <div className="max-w-2xl mx-auto p-8 text-center">Chart goes here</div>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+        />
+
+      </section>
+    </Layout>
+  )
+
+
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    return {
-        props: {}
-    }
+  return {
+    props: {}
+  }
 }
 
 
