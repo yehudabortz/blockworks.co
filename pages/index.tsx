68afcb2e56b0e6ae5e25f957e4ed5a07940c2530
Yehuda Bortz <yehudabortz@icloud.com>
2023-06-22 19:01:42 -0400

    remove 'data'

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,6 @@ export const getStaticProps: GetStaticProps = async () => {
     const res = await fetch('http://localhost:3000/api/btc-addresses');
     const data = await res.json()
     return {
-        props: { data: data }
+        props: { data }
     }
 }
